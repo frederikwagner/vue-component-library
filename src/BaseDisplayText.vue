87031831da1623a6c2d494e3201d07dd40e5a699
Frederik Badstue Wagner <fwexeter@gmail.com>
2019-05-03 19:40:32 +0200

    add display-text component

---
+++ src/BaseDisplayText.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="base-display-text" />
+</template>
+
+<script>
+export default {
+  name: "BaseDisplayText",
+}
+</script>
+
+<style lang="scss" scoped>
+
+$c: ".base-display-text";
+
+// #{$c} {}
+</style>
+
+<meta>
+{
+  "description": "A component that..."
+}
+</meta>
+
+<example>
+  <!-- No props or content are necessary. -->
+  <base-display-text></base-display-text>
+</example>
