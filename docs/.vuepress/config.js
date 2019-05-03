4255085d53ec3303679fbae3a7b63f3463335355
Frederik Badstue Wagner <fwexeter@gmail.com>
2019-05-03 22:25:15 +0200

    update docs structure and config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,39 @@
 module.exports = {
   title: 'Vue component library',
-  description: 'Vue component library documentation'
+  description: 'Vue component library documentation',
+  themeConfig: {
+    repo: 'frederikwagner/vue-component-library',
+    sidebar: [
+      ['/', 'Home'],
+      ['/inspiration', 'Inspiration'],
+      {
+        title: 'Creating your own',
+        collapsable: false,
+        children: [
+          ['/create/getting-started', 'Getting started'],
+          ['/create/development', 'Development'],
+          ['/create/project-structure', 'Project structure'],
+          ['/create/deployment', 'Deployment'],
+        ]
+      },
+      {
+        title: 'Using the library',
+        collapsable: false,
+        children: [
+          ['/use/getting-started', 'Getting started'],
+          ['/use/base-components', 'Base components'],
+          ['/use/layout-components', 'Layout components'],
+          ['/use/global-styles', 'Global styles'],
+        ]
+      },
+      {
+        title: 'Components',
+        collapsable: false,
+        children: [
+          ['/components/base-button', '<base-button>'],
+          ['/components/base-input', '<base-input>'],
+        ]
+      },
+    ],
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,17 @@
-# Getting started
+# Project documentation
 
-Welcome to the vue component library documentation.
+Welcome to the vue component library docs. This project is dedicated to those who want to create their own vue component library or use a standard vue component library in their web applications.
+
+## Table of contents
+
+- :wrench: Create your own component library.
+- :zap: Use the standard component library.
+- :blue_book: Standard components guide.
+
+## Acknowledgments
+
+This project is built on top of [Chris Fritz's](https://github.com/chrisvfritz) [example component library](https://github.com/chrisvfritz/hello-vue-components) and heavily inspired by his *must-read* [vue-enterprise-boilerplate](https://github.com/chrisvfritz/vue-enterprise-boilerplate) which is the gold-standard for Vue SPA projects. [This talk](https://youtu.be/7lpemgMhi0k) by Chris Fritz explains many of the patterns used and advocated in this project, and comes highly recommended - his energy is amazing!
+
+## Contributing
+
+To contribute to this project, please first discuss the change you wish to make via issue, email, or any other method with the owner of this repository before making a change.
