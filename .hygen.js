eaef3b9266d85f90d93eec98cf7823a66bb372cc
Frederik Badstue Wagner <fwexeter@gmail.com>
2019-05-04 09:24:49 +0200

    update and centralize component name helpers

--- .hygen.js
+++ .hygen.js
@@ -1,9 +1,16 @@
+const changeCase = require("change-case");
 const componentsConfig = require("./components.config.js");
+
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+const componentPrefix = componentsConfig.prefix.toLowerCase();
 
 module.exports = {
   helpers: {
     components: {
-      prefix: componentsConfig.prefix,
+      prefix: componentPrefix,
+      toKebabCase: name => componentPrefix + "-" + changeCase.snake(name).replace(/_/g, '-'),
+      toPascalCase: name =>
+        capitalize(componentPrefix) + changeCase.pascal(name),
     },
   }
 }
