e37f916ba1c02bbb5177f9840ace6a67be7f93e4
Frederik Badstue Wagner <fwexeter@gmail.com>
2019-05-08 14:28:04 +0100

    update buttons styles and props

--- src/BaseButton.vue
+++ src/BaseButton.vue
@@ -1,12 +1,117 @@
 <template>
-  <button class="base-button">
-    <slot />
+  <button
+    :class="buttonClasses"
+    v-bind="$attrs"
+    :aria-label="accessibilityLabel"
+    class="base-button"
+    v-on="$listeners"
+  >
+    <span class="button__content">
+      <slot />
+    </span>
   </button>
 </template>
 
 <script>
 export default {
   name: "BaseButton",
+  props: {
+    accessibilityLabel: {
+      type: String,
+      default: ""
+    },
+    size: {
+      type: String,
+      default: "medium",
+      validator: function(value) {
+        return [
+          "small",
+          "medium",
+          "large",
+        ].includes(value)
+      }
+    },
+    shape: {
+      type: String,
+      default: "rectangle",
+      validator: function(value) {
+        return [
+          "rectangle",
+          "square",
+          "circle",
+        ].includes(value)
+      }
+    },
+    type: {
+      type: String,
+      default: "basic",
+      validator: function(value) {
+        return [
+          "basic",
+          "primary",
+          "success",
+          "warning",
+          "danger",
+          "help",
+        ]
+      }
+    },
+    variation: {
+      type: String,
+      default: "basic",
+      validator: function(value) {
+        return [
+          "subtle",
+          "flat",
+          "inverted",
+        ].includes(value);
+      }
+    },
+    cap: {
+      type: [String, Array],
+      default: "",
+      validator: function(value) {
+        const validTypes = [
+          "",
+          "top",
+          "right",
+          "bottom",
+          "left",
+          "top-left",
+          "top-right",
+          "bottom-right",
+          "bottom-left"
+        ];
+
+        if (Array.isArray(value)) {
+          return value.some(type => validTypes.includes(type));
+        } else {
+          return validTypes.includes(value);
+        }
+      }
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  computed: {
+    buttonClasses() {
+      return {
+        "base-button--type-basic": this.type === "basic" || this.type === "",
+        "base-button--type-primary": this.type === "primary",
+        "base-button--type-success": this.type === "success",
+        "base-button--type-warning": this.type === "warning",
+        "base-button--type-danger": this.type === "danger",
+        "base-button--type-help": this.type === "help",
+        "base-button--size-small": this.size === "small",
+        "base-button--size-medium": this.size === "medium",
+        "base-button--size-large": this.size === "large",
+        "base-button--shape-square": this.shape === "square",
+        "base-button--shape-circle": this.shape === "circle",
+      }
+    }
+  }
 }
 </script>
 
